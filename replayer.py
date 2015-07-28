4bc468db6df31c9f11de5ae6b97a692ef6976cd6
Justin Caratzas <bigjust@lambdaphil.es>
2015-07-28 18:24:36 -0400

    initial commit
    currently just parses elb logs, doesn't do anything

---
+++ replayer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import argparse
+import sys
+
+import requests
+
+from twisted.internet import task
+from twisted.internet import reactor
+
+description='ELB Log Replayer (ELR)'
+
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('logfile', help='the logfile to replay')
+parser.add_argument('--host',
+                    help='host to send requests',
+                    default='localhost',
+                )
+
+script_args = parser.parse_args()
+
+def replay_request(url):
+    requests.get(url)
+
+def main():
+    for line in open(script_args.logfile):
+        bits = line.split()
+        timestamp = bits[0]
+        method = bits[11].lstrip('"')
+        url = bits[12]
+        if method != 'GET':
+            continue
+
+if __name__ == "__main__":
+    main()
